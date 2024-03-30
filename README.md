4adff21d0d6cbcaf0db1b2bed82d72d115cbf79f
idossha <idochaber@gmail.com>
2024-03-30 01:03:39 -0500

    update to vscode, readme, and installation script

--- README.md
+++ README.md
@@ -3,9 +3,21 @@
 Ido Haber
 Last update: March 27, 2024
 
-This is an adaptation (simplified version) of https://github.com/josean-dev/dev-environment-files by josean.
-
 Disclosure: This environment is in working development and far from perfect, so clone at your own discretion.
+
+---
+
+to keep in mind. when installing oh-my-zsh it often creates a new .zshrc -> make sure you use the old one from repo.
+
+make sure there is no .config that is created when opening the terminal that is conflicted with the .config created by symlink.
+
+last update included changes to installtion script that could possibly break. so install with caution.
+
+---
+
+for .vscode as of now only an extension.txt file.
+
+you call always pull the list of extenions by: - going to your terminal - running `code --list-extensions`
 
 ---
 
@@ -89,21 +101,9 @@ source ~/.zshrc
 
 ---
 
-### from josean:
-
-Terminal Setup + Relevant files:
-
-https://www.youtube.com/watch?v=CF1tMjvHDRA&list=PLnu5gT9QrFg36OehOdECFvxFFeMHhb_07&index=2&t=479s
-
-[.zshrc](.zshrc) - Zsh Shell Configuration
-
-#### Tmux Setup + Relevant files:
-
-https://youtu.be/U-omALWIBos
-
-[.tmux.conf](.tmux.conf) - Tmux Configuration File
-
 #### Neovim Setup + Relevant files:
+
+The neovim configuration is an adaptation (simplified version) of https://github.com/josean-dev/dev-environment-files by josean.
 
 https://youtu.be/6mxWayq-s9I
 [.config/nvim](.config/nvim)
--- create_symlinks.sh
+++ create_symlinks.sh
@@ -20,3 +20,63 @@ done
 
 echo "Symlinks created successfully."
 
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Ensure your system is ready to brew.
+echo "Running brew doctor..."
+brew doctor
+
+echo "Running brew update..."
+brew update
+
+# Install Xcode Command Line Tools
+xcode-select --install
+
+# Brew install packages
+BREW_CASK_PACKAGES=(
+  iterm2
+  rectangle
+)
+
+BREW_PACKAGES=(
+  tmux
+  git
+  neovim
+  ripgrep
+  node
+  jq
+)
+
+echo "Installing brew cask packages..."
+for package in "${BREW_CASK_PACKAGES[@]}"; do
+  brew install --cask $package
+done
+
+echo "Tapping into homebrew/cask-fonts..."
+brew tap homebrew/cask-fonts
+
+echo "Installing brew packages..."
+for package in "${BREW_PACKAGES[@]}"; do
+  brew install $package
+done
+
+echo "Installing font-hack-nerd-font..."
+brew install font-hack-nerd-font
+
+# Check for Oh My Zsh, install if we don't have it
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Install zsh-autosuggestions & zsh-syntax-highlighting
+ZSH_CUSTOM_PLUGINS="$HOME/.oh-my-zsh/custom/plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM_PLUGINS}/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM_PLUGINS}/zsh-syntax-highlighting
+
+echo "Script execution finished!"
+
