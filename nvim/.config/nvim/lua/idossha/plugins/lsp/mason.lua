2b5f422503fa3d4ea00691545c5afb749929c83e
Ido Haber <idohaber@Idos-MacBook-Pro-2.local>
2024-12-02 17:57:58 -0600

    added matlab_ls

--- nvim/.config/nvim/lua/idossha/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/idossha/plugins/lsp/mason.lua
@@ -37,6 +37,7 @@ return {
         "emmet_ls",
         "prismals",
         "pyright",
+        "matlab_ls"
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
