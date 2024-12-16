2f3c57be8e9fae89b0df9a0c599014d46d71b990
idossha <idochaber@gmail.com>
2024-12-16 14:29:00 -0600

    configured sketchybar to play nicely with office iMAC. need to make sure it doesnt break macbook

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -2,19 +2,37 @@
 #!/bin/bash
 
 # aerospace.sh
+
 sketchybar --add event aerospace_workspace_change
 
 for sid in $(aerospace list-workspaces --all); do
-    sketchybar --add item space.$sid left \
-        --subscribe space.$sid aerospace_workspace_change \
-        --set space.$sid \
-        background.color=0x44ffffff \
-        background.corner_radius=5 \
-        background.height=20 \
-        background.drawing=off \
-        label="$sid" \
-        padding_left=10 \
-        padding_right=10 \
-        click_script="aerospace workspace $sid" \
-        script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+    # Check if item already exists (requires SketchyBar 1.8.0+)
+    if ! sketchybar --query space.$sid &> /dev/null; then
+        # Item doesn't exist, add it
+        sketchybar --add item space.$sid left \
+            --subscribe space.$sid aerospace_workspace_change \
+            --set space.$sid \
+                background.color=0x44ffffff \
+                background.corner_radius=5 \
+                background.height=20 \
+                background.drawing=off \
+                label="$sid" \
+                padding_left=10 \
+                padding_right=10 \
+                click_script="aerospace workspace $sid" \
+                script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+    else
+        # Item exists, update its properties if needed
+        sketchybar --set space.$sid \
+            background.color=0x44ffffff \
+            background.corner_radius=5 \
+            background.height=20 \
+            background.drawing=off \
+            label="$sid" \
+            padding_left=10 \
+            padding_right=10 \
+            click_script="aerospace workspace $sid" \
+            script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+    fi
 done
+
--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -1,33 +1,56 @@
+
 #!/bin/bash
 
 source "$CONFIG_DIR/icons.sh"
 source "$CONFIG_DIR/colors.sh"
 
-BATTERY_INFO="$(pmset -g batt)"
+BATTERY_INFO="$(pmset -g batt 2>/dev/null)"
+
+# Check if no battery is present (common on desktops like iMacs)
+if echo "$BATTERY_INFO" | grep -qi "no battery"; then
+  # Hide the battery item since it's not applicable
+  sketchybar --set "$NAME" drawing=off
+  exit 0
+fi
+
 PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
-if [ $PERCENTAGE = "" ]; then
+# If PERCENTAGE is empty for some reason, hide the item
+if [ -z "$PERCENTAGE" ]; then
+  sketchybar --set "$NAME" drawing=off
   exit 0
 fi
 
 DRAWING=on
 COLOR=$WHITE
-case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
-  ;;
-  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
-  ;;
-  [3-5][0-9]) ICON=$BATTERY_50
-  ;;
-  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
-  ;;
-  *) ICON=$BATTERY_0; COLOR=$RED
+
+case $PERCENTAGE in
+  9[0-9]|100)
+    ICON=$BATTERY_100
+    DRAWING=off
+    ;;
+  [6-8][0-9])
+    ICON=$BATTERY_75
+    DRAWING=off
+    ;;
+  [3-5][0-9])
+    ICON=$BATTERY_50
+    ;;
+  [1-2][0-9])
+    ICON=$BATTERY_25
+    COLOR=$ORANGE
+    ;;
+  *)
+    ICON=$BATTERY_0
+    COLOR=$RED
+    ;;
 esac
 
-if [[ $CHARGING != "" ]]; then
+if [ -n "$CHARGING" ]; then
   ICON=$BATTERY_CHARGING
   DRAWING=off
 fi
 
-sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+sketchybar --set "$NAME" drawing=$DRAWING icon="$ICON" icon.color=$COLOR
+
