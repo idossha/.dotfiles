46dd10e98f1feab9f924c290c1dde948dfd914fe
idossha <idochaber@gmail.com>
2025-02-13 01:06:17 -0600

    added htop

--- nvim/.config/nvim/lua/idossha/plugins/init.lua
+++ nvim/.config/nvim/lua/idossha/plugins/init.lua
@@ -2,4 +2,8 @@ return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+  {
+    "myplugin",
+    dir = "~/Git-Projects/myplugin",
+  },
 }
