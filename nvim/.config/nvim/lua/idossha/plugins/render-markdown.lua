55b5576d87524f9e7d5debd48e6c4dc3bc65e4bb
idossha <idochaber@gmail.com>
2025-02-17 00:48:46 -0600

    improved markdown render visuals

--- nvim/.config/nvim/lua/idossha/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/idossha/plugins/render-markdown.lua
@@ -1,9 +1,19 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons'
+  },
+  enabled = true,
+  opts = {},
+  config = function()
+    require('render-markdown').setup({
+      heading = {
+        width = 'block',
+        left_pad = 2,
+        right_pad = 4,
+      },
+    })
+  end,
 }
+
