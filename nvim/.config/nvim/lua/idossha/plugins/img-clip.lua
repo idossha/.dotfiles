a6d041714563ecdc7cb6c614e50e2414701f9d77
idossha <idochaber@gmail.com>
2025-02-19 01:08:11 -0600

    removed img-clip keymaps

--- nvim/.config/nvim/lua/idossha/plugins/img-clip.lua
+++ nvim/.config/nvim/lua/idossha/plugins/img-clip.lua
@@ -5,8 +5,4 @@ return {
     -- add options here
     -- or leave it empty to use the default settings
   },
-  keys = {
-    -- suggested keymap
-    { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-  },
 }
