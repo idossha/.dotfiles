8b45d4d72abccea421606f701ab153971d5e8f62
idossha <idochaber@gmail.com>
2025-04-01 14:06:04 -0500

    removed blink for now

---
+++ nvim/.config/nvim/lua/idossha/test/blink.py
@@ -0,0 +1,12 @@
+import numpy as np
+import simnibs
+# creatwe a sample function to test blink.cmp capabilities
+
+
+def sample_function(a, b):
+    return a + b
+
+print(sample_function(1, 2))
+
+
+
