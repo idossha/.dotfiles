c34c972cf10602322b328c37351c19334e300a49
idossha <idochaber@gmail.com>
2025-04-07 13:55:54 -0500

    update to grug keymaps

--- nvim/.config/nvim/lua/idossha/plugins/grug.lua
+++ nvim/.config/nvim/lua/idossha/plugins/grug.lua
@@ -2,12 +2,28 @@ return {
   'MagicDuck/grug-far.nvim',
   config = function()
     -- optional setup call to override plugin options
-    -- alternatively you can set options with vim.g.grug_far = { ... }
     require('grug-far').setup({
       -- options, see Configuration section below
       -- there are no required options atm
       -- engine = 'ripgrep' is default, but 'astgrep' or 'astgrep-rules' can
       -- be specified
     });
+    
+    -- Basic keybindings for grug-far
+    vim.keymap.set('n', '<leader>rr', function() require('grug-far').open() end, 
+        { desc = 'Open grug-far find and replace' })
+    
+    -- For visual mode, pre-fill search with selection
+    vim.keymap.set('v', '<leader>rr', function() require('grug-far').with_visual_selection() end,
+        { desc = 'Find and replace with visual selection' })
+    
+    -- For searching within a visual selection range (this is the fixed mapping)
+    vim.keymap.set('v', '<leader>rw', function() 
+        require('grug-far').open({ visualSelectionUsage = 'operate-within-range' })
+    end, { desc = 'Find and replace within selection' })
+    
+    -- Kill current instance
+    vim.keymap.set('n', '<leader>rk', function() require('grug-far').kill_instance(0) end, 
+        { desc = 'Kill grug-far instance' })
   end
 }
