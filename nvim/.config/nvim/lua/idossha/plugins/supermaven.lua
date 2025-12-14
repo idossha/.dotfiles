0318e09101ee47c06c44c2079b691dffefa15c57
idossha <idochaber@gmail.com>
2025-12-13 23:59:36 -0600

    modified supermaven for on/off toggle

--- nvim/.config/nvim/lua/idossha/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/idossha/plugins/supermaven.lua
@@ -4,31 +4,17 @@ return {
   config = function()
     local supermaven = require("supermaven-nvim")
 
-    -- State variable to track on/off
-    local sm_enabled = true
+    -- Setup Supermaven
+    supermaven.setup({})
 
     -- Function to toggle Supermaven AI
     _G.toggle_supermaven = function()
-      sm_enabled = not sm_enabled
-      if sm_enabled then
-        print("Supermaven: ON")
-      else
-        print("Supermaven: OFF")
-      end
+      local api = require("supermaven-nvim.api")
+      api.toggle()
     end
 
     -- Keymap to toggle (leader + a + s)
     vim.keymap.set("n", "<leader>as", _G.toggle_supermaven, { desc = "Toggle Supermaven AI" })
-
-    -- Setup Supermaven with a wrapper to respect toggle
-    supermaven.setup({
-      on_attach = function(client, bufnr)
-        -- Optional: you can wrap completion or suggestions here if needed
-      end,
-      enabled = function()
-        return sm_enabled
-      end,
-    })
   end,
 }
 
