2a34e1e63f3b9e267b3f8b9f33bcec5444eb11f5
idossha <idochaber@gmail.com>
2026-03-10 20:10:31 -0500

    finally added noice.

---
+++ nvim/.config/nvim/lua/idossha/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    cmdline = {
+      enabled = true,
+      view = "cmdline_popup", -- floating popup (often centered)
+    },
+  },
+}
